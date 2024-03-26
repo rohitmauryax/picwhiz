f29a58bbc605963bf0a37d9c175c2ac1c1b45e21
rohitmauryax <rohitmaurya152@gmail.com>
2024-03-27 00:14:37 +0530

    Update public routes in authMiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/app", "/app/api/webhooks/clerk", "/app/api/webhooks/stripe"],
-  ignoredRoutes: ["/no-auth-in-this-route"],
+  publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"],
+  // ignoredRoutes: ["/no-auth-in-this-route"],
 });
 
 export const config = {
